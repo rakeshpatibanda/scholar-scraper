d00999379583a43126def0330d766d0e59ad8f21
rakeshpatibanda <rakesh.patibanda@rmit.edu.au>
2019-09-20 07:21:32 +1000

    added router and started creating the front end page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+let scholar = require('google-scholar-extended');
+
+var searchValue = `"thematic analysis" AND "games" AND "HCI" AND "game design"`;
+var result = {};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  scholar.search(searchValue)
+    .then(resultsObj => {
+      result = resultsObj;
+      console.log(result);
+  })
+
+  res.render('index', 
+    {  
+       title: 'Express', 
+       queryResult: result 
+    });
 });
 
 module.exports = router;
