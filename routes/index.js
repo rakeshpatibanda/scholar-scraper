db6d5610b41997defcda803789b1460ac9476f0a
Daniel Dao <guangdanny@gmail.com>
2019-09-28 23:12:10 +1000

    add await result

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-let scholar = require('google-scholar-extended');
+let scholar = require("google-scholar-extended");
 
 var result = {};
 
 /* GET home page. */
-router.get('/', async function (req, res, next) {
-
-  res.render('index', {
-    title: 'Express'
+router.get("/", async function(req, res, next) {
+  res.render("index", {
+    title: "Express"
   });
 });
 
-router.post("/searchScholarButton", async function (req, res, next) {
-
+router.post("/searchScholarButton", async function(req, res, next) {
   var searchValue = req.body.searchString;
 
   console.log(searchValue);
 
-  scholar.search(searchValue)
+  await scholar.search(searchValue)
     .then(resultsObj => {
       result = resultsObj;
       console.log(result);
